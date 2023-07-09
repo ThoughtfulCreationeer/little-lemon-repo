8c7fda3f19c737ecf6e9ef7754f54f3cc1ac1625
Thøger  Kappel <thogerkappel@MacBook-Pro-tilhrende-Thger.local>
2023-07-09 13:09:46 +0200

    This is an update on 09/07 containing, meta tags within the index.html file, samantic tags within the the app.js, the addition of photos within the public folder and finally the addition of the component SpecialsSection.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 import './App.css';
+import SpecialsSection from './SpecialsSection';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <header>
+        <nav>
+          <a href="home">
+            <img src="/logo-n-text.png" alt="Little Lomon's logo"/>
+          </a>
+          <ul>
+            <li>
+              <a href=''>Home</a>
+            </li>
+            <li>
+              <a href=''>About</a>
+            </li>
+            <li>
+              <a href=''>Menu</a>
+            </li>
+            <li>
+              <a href=''>Reservations</a>
+            </li>
+            <li>
+              <a href=''>Order Online</a>
+            </li>
+            <li>
+              <a href=''>Login</a>
+            </li>
+          </ul>
+        </nav>
+        <h1> Little lemon </h1>
+        <h2> Chicago </h2>
+        <p>This an awesome text giving insight into what fantastic food is being made at Little Lemon resturant</p>
+      </header>
+      <main>
+        <SpecialsSection />
+        <section>
+          <h2>Testimonial</h2>
+        </section>
+        <section>
+          <h2>Little lemon</h2>
+        </section>
+      </main>
+      <footer>
+      </footer>
+    </>
   );
 }
 
---
+++ src/SpecialsSection.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function SpecialsSection(){
+
+    //this is where I should write the code that will fetch the data I need of popularity, dishnames, prices and description to display the cards properly and take us to the promised lands
+    
+    return (
+        <section>
+            <h2>Specials</h2>
+            {/* Content to be rendered goes here */}
+        </section>
+    );
+};
+
+export default SpecialsSection
