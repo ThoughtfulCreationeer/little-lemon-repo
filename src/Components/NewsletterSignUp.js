99a0e3eb4c91670bf608265b0cdd34582734808b
Thøger  Kappel <thogerkappel@MacBook-Pro-tilhrende-Thger.local>
2023-10-04 07:43:53 +0200

    Did some miniscule eastetic changes

--- src/Components/NewsletterSignUp.js
+++ src/Components/NewsletterSignUp.js
@@ -1,3 +1,4 @@
+//This is a component that will be inserted into the footer and whereever a newsletter signup is needed.
 import React, { useState } from "react";
 import '../styles.scss';
 
@@ -25,14 +26,13 @@ function NewsletterSignUp(){
                 <input type="email" id="email" placeholder="Enter your email" value={email} onChange= {(e) => setEmail(e.target.value)} />
                 <button type="submit">Submit</button>
             </div>
-            <label className="checkbox" htmlFor="checkbox">
+            <label className="checkbox-wrapper" htmlFor="checkbox">
                 <input type="checkbox" id="checkbox" checked={isChecked} onChange={(e) => setIsChecked(e.target.checked)} />
-                <span className="custom-checkbox"></span>
                 I agree to recieve the newsletter.
             </label>
             <div className="TCE">
                 <div className="Terms-n-conditions">
-                    <p>Will be used in accordance with our</p> <a>terms and conditions</a>
+                    <p>Will be used in accordance with our</p> <a href="https://www.iubenda.com/en/help/2859-terms-and-conditions-when-are-they-needed">terms and conditions</a>
                 </div>
                 {errorMessage && <p className="error-message">{errorMessage}</p>}
             </div>
--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -8,8 +8,8 @@ function Menu() {
       <Navbar/>
       <main className='Reservations-Page-Container'>
         <section className='Main-Reservations-Section'>
-          <h2> Menu </h2>
-          <p>Where food is a celebration, stories are shared, and everyone is family. Let us fill your plates with warmth, your glasses with cheer, and your heart with the true spirit of Greek hospitality. Here, every moment is a memory waiting to be savored.</p>
+          <h2> Menu (Under construction) </h2>
+          <p>---</p>
         </section>
       </main>
       <Footer/>
